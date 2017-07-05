f1e80513e43aaf7516cb5e85503ebccea120bb42
moracarlos <moracarlos9@gmail.com>
2017-07-05 11:37:11 -0400

    Part One, vainilla react

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/app.css';
+
+import SearchBar from './components/SearchBar';
+import GifList from './components/GifList';
+
+import GifModal from './components/GifModal';
+
+
+import request from 'superagent';
+
+
+class App extends React.Component {
+	 constructor() {
+        super();
+
+        this.state = {
+            gifs: [],
+            selectedGif: null,
+            modalIsOpen: false
+        }
+
+        this.handleTermChange = this.handleTermChange.bind(this);
+        //Hay que enlazarlo para poder acceder a this.state dentro del metodo handleTermChange
+        //Otra opcion es llamarlo con fat-arrow
+    }
+
+    openModal(gif) {
+        this.setState({
+            modalIsOpen: true,
+            selectedGif: gif
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            modalIsOpen: false,
+            selectedGif: null
+        });
+    }
+
+	handleTermChange(term){
+		const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
+
+        request.get(url, (err, res) => {
+            this.setState({gifs: res.body.data})
+        });
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar onTermChange={this.handleTermChange}/>
+				<GifList gifs={this.state.gifs} 
+						 onGifSelect={selectedGif=> this.openModal(selectedGif)} /> 
+				<GifModal modalIsOpen={this.state.modalIsOpen}
+                          selectedGif={this.state.selectedGif}
+                          onRequestClose={ () => this.closeModal() } />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
